87219d3660e6bbb504311eed641e4f2ab4eccb3f
Chris Gibson <chrischarlesgibson@gmail.com>
2022-12-09 17:23:12 -0600

    added navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Card from "./components/Card";
+import PageContainer from "./PageContainer";
 
-const App = () => <Card />;
+const App = () => <PageContainer />;
 
 export default App;
