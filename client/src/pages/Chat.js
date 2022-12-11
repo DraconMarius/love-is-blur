4682efdd54a02fff30131f62f72da11056d0784c
luizborges146 <113717656+luizborges146@users.noreply.github.com>
2022-12-11 13:48:03 -0800

    Chat created, need to connect it to Appolo server

--- client/src/pages/Chat.js
+++ client/src/pages/Chat.js
@@ -1,39 +1,45 @@
-
 import io from 'socket.io-client';
 import { useState } from "react";
+import  Messages  from './Messages'
+
 const socket = io.connect("http://localhost:3002");
 
 function Chat() {
     const [username, setUsername] = useState("");
-    const [room, setRoom] = useState("");
+  const [room, setRoom] = useState("");
+  const [showChat, setShowChat] = useState(false); // only show the chat if the show chat is equal to true
 
-    const joinRoom = () => {
-        if (username !=="" & room !=="") {
-          socket.emit("join_room", room);
-        }
-      }
-
-
-    return (
-        <div>
-            <h3>Join the chat</h3>
-
-            <input type="text" placeholder="Luiz..." 
-            onChange={(event) =>{
-            setUsername(event.target.value)
-            }} 
-            />
-
-            <input type="text" placeholder="Room ID..." 
-            onChange={(event) =>{
-            setRoom(event.target.value)
-            }} 
-            />    
-
-            <button>Join a Room</button>
-
-        </div>
-    )
+const joinRoom = () => {
+  if (username !=="" & room !=="") {
+    socket.emit("join_room", room);
+    setShowChat(true);
+  }
 }
 
+  return (
+    <div className="App">
+      {!showChat ? (
+      <div className="joinChatcontainer">
+        <h3>Join the chat</h3>
+        <input type="text" placeholder="Luiz..." 
+        onChange={(event) =>{
+          setUsername(event.target.value)
+          }} 
+          />
+        <input type="text" placeholder="Room ID..." 
+        onChange={(event) =>{
+          setRoom(event.target.value)
+          }} 
+          />
+        <button onClick={joinRoom}>Join a Room</button>
+
+      </div>
+      )
+      : (
+
+     <Messages socket={socket} username={username} room={room}/>
+  )}
+    </div>
+  );
+}
 export default Chat;
---
+++ client/src/pages/Messages.js
@@ -0,0 +1,72 @@
+import React, { useEffect, useState } from 'react'
+
+export default function Messages({socket, username, room}) {
+    const [currentMessage, setCurrentMessage] = useState("");
+    // display the chat to the user
+    const [messageList, setMessageList] = useState([]);
+
+    const sendMessage = async () => {
+        if (currentMessage !== "") {
+            const messageData = {
+                room: room,
+                author: username,// we can call this user name, i just thought that could be confusing
+                message:currentMessage,
+                time: // to generate the time the user get the message
+                    new Date(Date.now()).getHours() +
+                    ":" +
+                    new Date(Date.now()).getMinutes(),
+             };
+
+             await socket.emit("send_message", messageData);
+             setMessageList((list) => [...list,messageData]);
+             setCurrentMessage(""); //set the current message to be empty
+        }
+    };
+
+    useEffect(()=> {
+        socket.on("receive_message", (data) => {// receive the DATA from the back end index.js server
+            // console.log(data);
+            // setMessageList((list) => [...list,data])
+            setMessageList((messageList) => [...messageList,data])
+        })
+        return () => {
+            socket.off("receive_message");
+        }
+    }, [socket]);
+
+  return (
+    <div className="chat-window">
+        <div className="chat-header">
+            <p>Live Chat</p>
+        </div>
+        <div className="chat-body">
+            {messageList.map((messageContent, index)=> {
+                return <div key={index} className="message" id={username === messageContent.author ? "you" : "other"}>
+                    <div>
+                        <div className="message-content">
+                            <p>{messageContent.message}</p>
+                        </div>
+
+                        <div className="message-meta">
+                            <p id="time">{messageContent.time}</p>
+                            <p id="author">{messageContent.author}</p>
+                        </div>
+
+                    </div>
+                </div>
+            })}
+        </div>
+        <div className="chat-footer">
+            <input type="text" placeholder='Hey...' 
+                onChange={(event) =>{
+                setCurrentMessage(event.target.value);
+            }} 
+            onKeyDown={(event) => {// added Enter key to be listen
+                event.key === "Enter" && sendMessage();
+            }}
+            />
+            <button onClick={sendMessage}>Send</button>
+        </div>
+    </div>
+  )
+}
