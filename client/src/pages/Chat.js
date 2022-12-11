1fa87af80c9dd451317731e581f682ffb479447f
luizborges146 <113717656+luizborges146@users.noreply.github.com>
2022-12-11 13:41:35 -0800

    Added the socket.io in the server, so it can comunicate with the client side

--- client/src/pages/Chat.js
+++ client/src/pages/Chat.js
@@ -1,16 +1,34 @@
 
 import io from 'socket.io-client';
 import { useState } from "react";
-// const socket = io.connect("http://localhost:3001");
+const socket = io.connect("http://localhost:3002");
 
 function Chat() {
+    const [username, setUsername] = useState("");
+    const [room, setRoom] = useState("");
+
+    const joinRoom = () => {
+        if (username !=="" & room !=="") {
+          socket.emit("join_room", room);
+        }
+      }
+
+
     return (
         <div>
             <h3>Join the chat</h3>
 
-            <input type="text" placeholder="Luiz..."/>
+            <input type="text" placeholder="Luiz..." 
+            onChange={(event) =>{
+            setUsername(event.target.value)
+            }} 
+            />
 
-            <input type="text" placeholder="Room ID..."  />
+            <input type="text" placeholder="Room ID..." 
+            onChange={(event) =>{
+            setRoom(event.target.value)
+            }} 
+            />    
 
             <button>Join a Room</button>
 
--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const { authMiddleware } = require('./utils/auth');
 
-// const http = require("http"); // required to build the server with socket.io
+const http = require("http"); // required to build the server with socket.io
 const cors = require("cors"); // socket.io deal with some of the socket.io issue
 const { Server} = require("socket.io")// It will handle the messages to send and receive + the socket id 
 
@@ -19,25 +19,43 @@ const server = new ApolloServer({
   context: authMiddleware,
 });
 
-/*####################### Created by Luiz #################################################*/
+/*####################### Created by Luiz ##################################*/
 app.use(cors());
 
-const io = new Server(server, {
+const serverSocket = http.createServer(app);
+
+const io = new Server(serverSocket, {
   cors: {
       origin: "http://localhost:3000",
       methods: ["GET", "POST"],
   }
 });
 
-io.on("connection", (socket) => {
+io.on("connection", (socket) => {//listen for an event
   console.log(`User connected: ${socket.id}`);
+  //socket.on listen to the events
+  socket.on("join_room", (data) => {// to join a room
+      socket.join(data);
+      console.log (`User with ID: ${socket.id} join room: ${data}`);
+  })
+
+  socket.on("send_message", (data) =>{// get the user message in the room
+      //data.room will separete the messages per room
+      socket.to(data.room).emit("receive_message",data)
+      // console.log(data);
+
+  })
 
   socket.on ("disconnect", () => { // disconnect a user 
-    console.log("User disconnected", socket.id)
-})
+      console.log("User disconnected", socket.id)
+  })
 });
 
-/*####################### Created by Luiz #################################################*/
+serverSocket.listen(3002, () => {
+  console.log("Server running")
+} );
+
+/*####################### Created by Luiz ##############################*/
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
