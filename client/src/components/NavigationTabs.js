61dfb2b4fa81520cbac4482ef8e89546780af981
Chris Gibson <chrischarlesgibson@gmail.com>
2022-12-12 12:22:00 -0600

    fixed navbar so that the logout/singup and login tabs are at the end

--- client/src/components/NavigationTabs.js
+++ client/src/components/NavigationTabs.js
@@ -6,63 +6,41 @@ import { useState } from "react";
 import Auth from "../utils/auth";
 
 /* eslint-disable jsx-a11y/anchor-is-valid */
-function NavigationTabs({ handlePageChange }) {
+function NavigationTabs() {
   const [isActive, setisActive] = useState(false);
   return (
-    <nav className="navbar" role="navigation" aria-label="main navigation">
-      <div className="navbar-brand">
-        <a
-          onClick={() => {
-            setisActive(!isActive);
-          }}
-          role="button"
-          className={`navbar-burger burger ${isActive ? "is-active" : ""}`}
-          aria-label="menu"
-          aria-expanded="false"
-          data-target="navbarBasicExample"
-        >
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-        </a>
-      </div>
-
-      <div
-        id="navbarBasicExample"
-        className={`navbar-menu ${isActive ? "is-active" : ""}`}
-      >
-        <div className="navbar-start">
-          <motion.div
-            className="nav-link"
-            whileHover={{ scale: 1.1 }}
-            whileTap={{ scale: 0.9 }}
-          >
-            <Link
-              to="/home"
+    <>
+      {Auth.loggedIn() ? (
+        <nav className="navbar" role="navigation" aria-label="main navigation">
+          <div className="navbar-brand">
+            <a
+              onClick={() => {
+                setisActive(!isActive);
+              }}
+              role="button"
+              className={`navbar-burger burger ${isActive ? "is-active" : ""}`}
+              aria-label="menu"
+              aria-expanded="false"
+              data-target="navbarBasicExample"
             >
-              Home
-            </Link>
-          </motion.div>
-          {Auth.loggedIn() ? (
-            <>
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+            </a>
+          </div>
+
+          <div
+            id="navbarBasicExample"
+            className={`navbar-menu ${isActive ? "is-active" : ""}`}
+          >
+            <div className="navbar-start">
               <motion.div
                 className="nav-link"
                 whileHover={{ scale: 1.1 }}
                 whileTap={{ scale: 0.9 }}
               >
-                <Link to="/chat">
-                  Chat
-                </Link>
-              </motion.div>
-              <motion.div
-                className="nav-link"
-                whileHover={{ scale: 1.1 }}
-                whileTap={{ scale: 0.9 }}
-              >
-                <Link to="/profile">
-                  profile
-                </Link>
+                <Link to="/home">Home</Link>
               </motion.div>
 
               <motion.div
@@ -70,12 +48,75 @@ function NavigationTabs({ handlePageChange }) {
                 whileHover={{ scale: 1.1 }}
                 whileTap={{ scale: 0.9 }}
               >
-                <Link to="/swipe">
-                  Swipe
-                </Link>
+                <Link to="/chat">Chat</Link>
               </motion.div>
+              <motion.div
+                className="nav-link"
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.9 }}
+              >
+                <Link to="/profile">profile</Link>
+              </motion.div>
+
+              <motion.div
+                className="nav-link"
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.9 }}
+              >
+                <Link to="/swipe">Swipe</Link>
+              </motion.div>
+            </div>
+            <>
+              <div className="navbar-end">
+                <div className="navbar-item">
+                  <div className="buttons">
+                    <motion.div
+                      className="logout"
+                      whileHover={{ scale: 1.1 }}
+                      whileTap={{ scale: 0.9 }}
+                    >
+                      <Link onClick={Auth.logout}>Logout</Link>
+                    </motion.div>
+                  </div>
+                </div>
+              </div>
             </>
-          ) : (
+          </div>
+        </nav>
+      ) : (
+        <nav className="navbar" role="navigation" aria-label="main navigation">
+          <div className="navbar-brand">
+            <a
+              onClick={() => {
+                setisActive(!isActive);
+              }}
+              role="button"
+              className={`navbar-burger burger ${isActive ? "is-active" : ""}`}
+              aria-label="menu"
+              aria-expanded="false"
+              data-target="navbarBasicExample"
+            >
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+            </a>
+          </div>
+
+          <div
+            id="navbarBasicExample"
+            className={`navbar-menu ${isActive ? "is-active" : ""}`}
+          >
+            <div className="navbar-start">
+              <motion.div
+                className="nav-link"
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.9 }}
+              >
+                <Link to="/home">Home</Link>
+              </motion.div>
+            </div>
+
             <>
               <div className="navbar-end">
                 <div className="navbar-item">
@@ -85,30 +126,23 @@ function NavigationTabs({ handlePageChange }) {
                       whileHover={{ scale: 1.1 }}
                       whileTap={{ scale: 0.9 }}
                     >
-                      <Link
-                        to="/signup">
-                        Sign Up
-                      </Link>
+                      <Link to="/signup">Sign Up</Link>
                     </motion.div>
                     <motion.div
                       className="login"
                       whileHover={{ scale: 1.1 }}
                       whileTap={{ scale: 0.9 }}
                     >
-                      <Link
-                        to="/login">
-                        Log in
-                      </Link>
+                      <Link to="/login">Log in</Link>
                     </motion.div>
                   </div>
                 </div>
               </div>
             </>
-          )}
-        </div>
-      </div>
-    </nav >
+          </div>
+        </nav>
+      )}
+    </>
   );
 }
-
 export default NavigationTabs;
