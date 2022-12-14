85e42eeef6ff6f66a3eb5b9dfeb56e0d199ae615
JCaloca <jacobcaloca1@gmail.com>
2022-12-14 15:20:45 -0800

    small fix

--- server/server.js
+++ server/server.js
@@ -1,16 +1,16 @@
-const express = require("express");
+const express = require('express');
 
-const { ApolloServer } = require("apollo-server-express");
-const path = require("path");
+const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
 // reqquiring middleware to verify the token
-const { authMiddleware } = require("./utils/auth");
+const { authMiddleware } = require('./utils/auth');
 
-const http = require("http"); // required to build the server with socket.io
-const cors = require("cors"); // socket.io deal with some of the socket.io issue
-const socketio = require("socket.io"); // It will handle the messages to send and receive + the socket id
+const http = require('http'); // required to build the server with socket.io
+const cors = require('cors'); // socket.io deal with some of the socket.io issue
+const socketio = require('socket.io'); // It will handle the messages to send and receive + the socket id
 // require the typeDefs and resolvers
-const { typeDefs, resolvers } = require("./schemas");
-const db = require("./config/connection");
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
 // use port 3001 if it's not defined
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -64,12 +64,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Serve up static assets
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
 }
 // send every other request to the React
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 // Create a new instance of an Apollo server with the GraphQL schema
@@ -78,7 +78,7 @@ const startApolloServer = async (typeDefs, resolvers) => {
   // integrate our Apollo server with the Express application as middleware
   server.applyMiddleware({ app });
   //   // start the  server
-  db.once("open", () => {
+  db.once('open', () => {
     const httpServer = app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
       console.log(
@@ -89,27 +89,33 @@ const startApolloServer = async (typeDefs, resolvers) => {
     //attaching our app with socket.io by luiz
     const io = socketio(httpServer);
 
-    io.on("connection", (socket) => {
+    io.on('connection', (socket) => {
       //listen for an event
       console.log(`User connected: ${socket.id}`);
       //socket.on listen to the events
-      socket.on("join_room", (data) => {
+      socket.on('join_room', (data) => {
         // to join a room
         socket.join(data);
         console.log(`User with ID: ${socket.id} join room: ${data}`);
       });
 
-      socket.on("send_message", (data) => {
+      socket.on('send_message', (data) => {
         // get the user message in the room
         //data.room will separete the messages per room
-        console.log("server-side", data);
-        socket.to(data.chatId).emit("receive_message", data)
-        console.log(data.messages.messageAuthor + " with socketID of " + socket.id + " said " + data.messages.messageText);
+        console.log('server-side', data);
+        socket.to(data.chatId).emit('receive_message', data);
+        console.log(
+          data.messages.messageAuthor +
+            ' with socketID of ' +
+            socket.id +
+            ' said ' +
+            data.messages.messageText
+        );
 
-
-      socket.on("disconnect", () => {
-        // disconnect a user
-        console.log("User disconnected", socket.id);
+        socket.on('disconnect', () => {
+          // disconnect a user
+          console.log('User disconnected', socket.id);
+        });
       });
     });
   });
