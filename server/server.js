a2dbe98fef420bb4d955adfdb0d48c339308fc7d
DraconMarius <113488982+DraconMarius@users.noreply.github.com>
2022-12-15 08:56:15 -0800

    attempting fix for heroku reload bug

--- server/server.js
+++ server/server.js
@@ -68,7 +68,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 // send every other request to the React
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
